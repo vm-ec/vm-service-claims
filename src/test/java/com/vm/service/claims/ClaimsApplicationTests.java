1045f4d42cc2f3026cc72d2c78888005b91cd1c7
Ajay Yenugula <ajay.yenugula@valuemomentum.com>
2025-11-18 11:44:30 +0530

    first commit

---
+++ src/test/java/com/vm/service/claims/ClaimsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vm.service.claims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaimsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
