4f42959cfde0d818a7be7b7f570c6c0fad2d5b64
Ajay Yenugula <ajay.yenugula@valuemomentum.com>
2025-11-19 15:28:27 +0530

    added repository

--- src/main/java/com/vm/service/claims/repo/ClaimsRepository.java
+++ src/main/java/com/vm/service/claims/repo/ClaimsRepository.java
@@ -1,9 +1,11 @@
 package com.vm.service.claims.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.vm.service.claims.entitys.Claims;
 
+@Repository
 public interface ClaimsRepository extends JpaRepository<Claims,Long> {
 
 }
